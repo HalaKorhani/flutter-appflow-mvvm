aae8fa52a83526da58d4d2286012dc3588b090f0
HalaKorhani <Halakorhani2005@icloud.com>
2026-06-20 21:07:05 +0300

    Add app reset repository

---
+++ lib/repositories/app_reset_repository.dart
@@ -0,0 +1,13 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+// Repository responsible for clearing all saved local data.
+// We use it during logout to reset Remember Me, timer, selected image, etc.
+class AppResetRepository {
+  Future<void> clearAllSavedValues() async {
+    // Get the SharedPreferences instance.
+    final prefs = await SharedPreferences.getInstance();
+
+    // Clear all saved values from local storage.
+    await prefs.clear();
+  }
+}

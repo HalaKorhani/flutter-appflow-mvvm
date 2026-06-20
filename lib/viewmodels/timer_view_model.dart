8efbd8cc5c7ce6c1ec05b9961ba9a5a827e9ee3a
HalaKorhani <Halakorhani2005@icloud.com>
2026-06-20 22:02:15 +0300

    Create timer ViewModel state

---
+++ lib/viewmodels/timer_view_model.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import '../repositories/timer_repository.dart';
+import '../utils/date_time_utils.dart';
+
+// ViewModel responsible for timer state and timer logic.
+class TimerViewModel extends ChangeNotifier {
+  final TimerRepository _repository;
+
+  TimerViewModel(this._repository);
+
+  Timer? _timer;
+  int _totalSeconds = 0;
+  bool _initialized = false;
+
+  int get totalSeconds => _totalSeconds;
+  String get formattedTime => DateTimeUtils.formatDuration(_totalSeconds);
+}

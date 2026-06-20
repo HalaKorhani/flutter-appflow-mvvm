5a3a3d8e658b771ebdecd9979db92b76cc1ce5b6
HalaKorhani <Halakorhani2005@icloud.com>
2026-06-20 21:09:34 +0300

    Add storage keys utility

---
+++ lib/repositories/auth_repository.dart
@@ -0,0 +1,8 @@
+// Central place for SharedPreferences keys.
+// This avoids spelling mistakes when saving/loading values.
+class StorageKeys {
+  static const rememberMe = 'remember_me';
+  static const firstLoginDate = 'first_login_date';
+  static const timerSeconds = 'timer_seconds';
+  static const selectedImagePath = 'selected_image_path';
+}

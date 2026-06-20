425eb5d89dfbb909f3549dfad5f7b7cf47a26e3f
HalaKorhani <Halakorhani2005@icloud.com>
2026-06-20 21:59:13 +0300

    Add timer duration formatter

--- lib/utils/date_time_utils.dart
+++ lib/utils/date_time_utils.dart
@@ -6,4 +6,15 @@ class DateTimeUtils {
   static String formatFirstLogin(DateTime dateTime) {
     return DateFormat('dd/MM/yyyy HH:mm').format(dateTime);
   }
+
+  // Converts total seconds to hh:mm:ss.
+  static String formatDuration(int totalSeconds) {
+    final hours = totalSeconds ~/ 3600;
+    final minutes = (totalSeconds % 3600) ~/ 60;
+    final seconds = totalSeconds % 60;
+
+    String twoDigits(int value) => value.toString().padLeft(2, '0');
+
+    return '${twoDigits(hours)}:${twoDigits(minutes)}:${twoDigits(seconds)}';
+  }
 }

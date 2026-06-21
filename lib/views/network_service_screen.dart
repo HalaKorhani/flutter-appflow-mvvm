edd95408672d96bb752056fa8b0cb5b6a31380e1
HalaKorhani <Halakorhani2005@icloud.com>
2026-06-21 14:17:57 +0300

    Connect network screen to ViewModel

--- lib/views/network_service_screen.dart
+++ lib/views/network_service_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../viewmodels/network_service_view_model.dart';
 
 // Screen responsible for showing network monitoring feature.
 class NetworkServiceScreen extends StatelessWidget {
@@ -6,12 +9,15 @@ class NetworkServiceScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(20),
+    final viewModel = context.watch<NetworkServiceViewModel>();
+
+    return Padding(
+      padding: const EdgeInsets.all(20),
       child: Center(
         child: Text(
-          'Network Service Screen',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          viewModel.currentStatus,
+          textAlign: TextAlign.center,
+          style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
         ),
       ),
     );
